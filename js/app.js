29fa023b772d0b521a2652af10651e75a984d8a9
Connor Crossley <cacrossley22@gmail.com>
2018-06-25 14:16:07 -0700

    Add first 3 image objects to product image array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 'use strict';
+console.log('hi');
+/***********************************
+*         Global Variables         *
+************************************/
+
+var productOptionOneLabel = document.getElementById('productOptionOne');
+var productOptionTwoLabel = document.getElementById('productOptionTwo');
+var productOptionThreeLabel = document.getElementById('productOptionThree');
+var resultsTable = document.getElementById('results');
+
+/***********************************
+*      Product Image Object        *
+************************************/
+function ProductImage(name, src, id) {
+  this.name = name;
+  this.src = src;
+  this.id = id;
+  this.numTimesClicked = 0;
+  this.numTimesDisplayed = 0;
+  this.selectionPercentage;
+  ProductImage.productImageArray.push(this);
+};
+
+ProductImage.productImageArray = [];
+
+new ProductImage('Bag', './img/bag.jpg', 'bagImg');
+new ProductImage('Banana', './img/banana.jpg', 'bananaImg');
+new ProductImage('Bathroom', './img/bathroom.jpg', 'bathroomImg');
