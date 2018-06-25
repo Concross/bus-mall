44cc625f85d0462bf36f7f6a9e50d8d7d3989c23
Connor Crossley <cacrossley22@gmail.com>
2018-06-25 16:09:46 -0700

    Random images working and rendering on screen

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-console.log('hi');
 /***********************************
 *         Global Variables         *
 ************************************/
@@ -23,7 +22,71 @@ function ProductImage(name, src, id) {
 };
 
 ProductImage.productImageArray = [];
+ProductImage.pastSelectionArray = [];
+
+ProductImage.renderRandomThree = function () {
+
+  pickRandomThree();
+  productOptionOneLabel.src = ProductImage.productImageArray[ProductImage.pastSelectionArray[0]].src;
+  productOptionTwoLabel.src = ProductImage.productImageArray[ProductImage.pastSelectionArray[1]].src;
+  productOptionThreeLabel.src = ProductImage.productImageArray[ProductImage.pastSelectionArray[2]].src;
+
+};
 
 new ProductImage('Bag', './img/bag.jpg', 'bagImg');
 new ProductImage('Banana', './img/banana.jpg', 'bananaImg');
 new ProductImage('Bathroom', './img/bathroom.jpg', 'bathroomImg');
+new ProductImage('Boots', './img/boots.jpg', 'bootsImg');
+new ProductImage('Breakfast', './img/breakfast.jpg', 'breakfastImg');
+new ProductImage('Bubblegum', './img/bubblegum.jpg', 'bubblegumImg');
+new ProductImage('Chair', './img/chair.jpg', 'chairImg');
+new ProductImage('Cthulhu', './img/cthulhu.jpg', 'cthulhuImg');
+new ProductImage('Dog Duck', './img/dog-duck.jpg', 'dogDuckImg');
+new ProductImage('Dragon', './img/dragon.jpg', 'dragonImg');
+new ProductImage('Pen', './img/pen.jpg', 'penImg');
+ProductImage.renderRandomThree();
+/***********************************
+*         Helper Functions         *
+************************************/
+
+function pickRandomThree() {
+
+  do {
+    var randNum1 = Math.floor(Math.random() * ProductImage.productImageArray.length);
+  } while (ProductImage.pastSelectionArray.includes(randNum1));
+
+  do {
+    var randNum2 = Math.floor(Math.random() * ProductImage.productImageArray.length);
+  } while (randNum2 === randNum1 || ProductImage.pastSelectionArray.includes(randNum2));
+
+  do {
+    var randNum3 = Math.floor(Math.random() * ProductImage.productImageArray.length);
+  } while (randNum3 === randNum2 || randNum3 === randNum1 || ProductImage.pastSelectionArray.includes(randNum3));
+
+  ProductImage.pastSelectionArray = [randNum1, randNum2, randNum3];
+
+  return ProductImage.pastSelectionArray;
+
+}
+
+// var pastSelection1 = 0;
+// var pastSelection2 = 4;
+
+// GoatImage.renderTwoRandomly = function () {
+//   do {
+//     var randomNumber1 = Math.floor(Math.random() * GoatImage.goatArray.length);
+//   } while(randomNumber1 == pastSelection1 || randomNumber1 == pastSelection2);
+
+//   do {
+//     var randomNumber2 = Math.floor(Math.random() * GoatImage.goatArray.length);
+//   } while (randomNumber2 === randomNumber1 || randomNumber2 === pastSelection1 || randomNumber2 === pastSelection2);
+
+//   goatImage1.src = GoatImage.goatArray[randomNumber1].src;
+//   goatImage2.src = GoatImage.goatArray[randomNumber2].src;
+
+//   goatImage1.dataset.index = randomNumber1;
+//   goatImage2.dataset.index = randomNumber2;
+
+//   pastSelection1 = randomNumber1;
+//   pastSelection2 = randomNumber2;
+// };
